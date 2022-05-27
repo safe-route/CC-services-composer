1ced07d0643931f71694e9e0ccc4edf2bd7799d6
I Putu Cahya Adi Ganesha <iputucaganesha@gmail.com>
2022-05-27 21:27:08 +0700

    Added new resources
    Added the public facing website, JWT auth server and MongoDB

--- get-repositories.sh
+++ get-repositories.sh
@@ -2,3 +2,5 @@
 
 git clone https://gitlab.com/safe-route/web-app-sandbox.git
 git clone https://gitlab.com/safe-route/backend-servers.git
+https://gitlab.com/safe-route/cloud-computing/saferoute-jwt-express.git
+https://gitlab.com/safe-route/cloud-computing/front-facing-website.git
